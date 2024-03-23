d699d20acc779faf85a7c4e4d47f0e6ca95dc925
SOOSOO37 <115699805+SOOSOO37@users.noreply.github.com>
2024-03-23 23:57:34 +0900

    refactor:recruitPostController log추가

--- server/src/main/java/com/seb006/server/recruitpost/controller/RecruitPostController.java
+++ server/src/main/java/com/seb006/server/recruitpost/controller/RecruitPostController.java
@@ -12,6 +12,8 @@ import com.seb006.server.recruitpost.entity.RecruitPost;
 import com.seb006.server.recruitpost.mapper.RecruitPostMapper;
 import com.seb006.server.recruitpost.service.RecruitPostService;
 import com.seb006.server.utils.UriCreator;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,7 +25,8 @@ import javax.validation.constraints.Positive;
 import java.net.URI;
 import java.util.List;
 
-
+@Slf4j
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/recruit-posts")
 public class RecruitPostController {
@@ -37,20 +40,16 @@ public class RecruitPostController {
     private final RecruitPostMapper mapper;
     private final LikeService likeService;
 
-    public RecruitPostController(RecruitPostService service, MemberService memberService, RecruitPostMapper mapper, LikeService likeService) {
-        this.service = service;
-        this.memberService = memberService;
-        this.mapper = mapper;
-        this.likeService = likeService;
-    }
-
     @PostMapping
     public ResponseEntity postRecruitPost(@AuthenticationPrincipal Member member,
                                           @RequestBody RecruitCreateDto recruitCreateDto){
 
+        log.info("Creating RecruitPost");
         RecruitPost recruitPost = service.createRecruitPost(member,mapper.recruitPostDtoToRecruitPost(recruitCreateDto));
 
         URI location = UriCreator.createUri(RECRUITPOST_DEFAULT_URL,recruitPost.getId());
+
+        log.info("RecruitPost ID: {}", recruitPost.getId());
 
         return ResponseEntity.created(location).build();
     }
@@ -58,15 +57,18 @@ public class RecruitPostController {
     @PatchMapping("/{recruit-post-id}")
     public ResponseEntity patchRecruitPost(@PathVariable("recruit-post-id")long id,
                                            @RequestBody RecruitPostUpdateDto recruitPostUpdateDto){
+        log.info("Updating RecruitPost : {}",id);
         recruitPostUpdateDto.setId(id);
         RecruitPost recruitPost = service.updateRecruitPost(mapper.recruitPostPatchDtoToRecruitPost(recruitPostUpdateDto));
+        log.info("Updated RecruitPost : {}",id);
 
         return new ResponseEntity<>(mapper.recruitPostToRecruitPostResponseDto(recruitPost),HttpStatus.OK);
     }
 
     @GetMapping("/{recruit-post-id}")
-    public ResponseEntity getRecruitPost(@AuthenticationPrincipal Member member, @PathVariable("recruit-post-id") long id){
-
+    public ResponseEntity getRecruitPost(@AuthenticationPrincipal Member member,
+                                         @PathVariable("recruit-post-id") long id){
+        log.info("Inquiring RecruitPost : {}",id);
         RecruitPost recruitPost = service.findRecruitPost(id);
 
         RecruitPostDetailResponseDto result = mapper.recruitPostToRecruitDetailResponseDto(recruitPost);
@@ -74,7 +76,7 @@ public class RecruitPostController {
         if(likeService.isRecruitPostLiked(member, recruitPost)){
             result.setLiked(true);
         }
-
+        log.info("Found RecruitPost : {}",id);
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
@@ -83,6 +85,7 @@ public class RecruitPostController {
                                           @Positive @RequestParam(defaultValue = "1") int page,
                                           @Positive @RequestParam(defaultValue = "10") int size,
                                           @Positive @RequestParam(defaultValue = "1") int sorting){
+        log.info("Inquiring All RecruitPost");
 
         Page<RecruitPost> recruitPostPage = service.findRecruitPosts(page-1,size,sorting);
         List<RecruitPost> recruitPosts = recruitPostPage.getContent();
@@ -94,7 +97,7 @@ public class RecruitPostController {
         List<Long> likedRecruitIds = likeService.recruitPostLiked(member, recruitPosts);
         List<RecruitPostResponseDto> result = mapper.recruitPostsToRecruitPostResponseDtos(recruitPosts,likedRecruitIds);
 
-
+        log.info("All RecruitPost : {}",recruitPostPage.getTotalElements());
         return new ResponseEntity<>(
                 new MultiResponseDto<>(mapper.recruitPostsToRecruitPostResponseDtos(recruitPosts, likedRecruitIds),recruitPostPage),
                 HttpStatus.OK);
@@ -103,7 +106,9 @@ public class RecruitPostController {
     //모집글 삭제
     @DeleteMapping("/{recruit-post-id}")
     public ResponseEntity deleteRecruitPost(@PathVariable("recruit-post-id")long id){
+        log.info("Deleting RecruitPost",id);
         service.deleteRecruitPost(id);
+        log.info("Deleted RecruitPost",id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
@@ -115,7 +120,7 @@ public class RecruitPostController {
                                               @RequestParam(defaultValue = "1") int sorting,
                                               @RequestParam(required = false, defaultValue = "") String category,
                                               @RequestParam(required = false, defaultValue = "") String keyword){
-
+        log.info("Inquiring Category & Keyword");
         Page<RecruitPost> recruitPostPage = service.searchRecruitPosts(page-1,size,sorting,category,keyword);
         List<RecruitPost> recruitPosts = recruitPostPage.getContent();
 
@@ -125,7 +130,7 @@ public class RecruitPostController {
         }
         List<Long> likedRecruitIds = likeService.recruitPostLiked(member, recruitPosts);
         List<RecruitPostResponseDto> result = mapper.recruitPostsToRecruitPostResponseDtos(recruitPosts,likedRecruitIds);
-
+        log.info("Found Category & Keyword");
 
         return new ResponseEntity<>(
                 new MultiResponseDto<>(mapper.recruitPostsToRecruitPostResponseDtos(recruitPosts, likedRecruitIds),recruitPostPage),
@@ -136,14 +141,18 @@ public class RecruitPostController {
     //모집글 닫기
     @PatchMapping("/{recruit-post-id}/close")
     public ResponseEntity closeRecruitPost (@PathVariable("recruit-post-id") long id) {
+        log.info("Closing RecruitPost :{}",id);
         service.closeRecruitPost(id);
+        log.info("Closed RecruitPost :{}",id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     //모집 실패
     @PatchMapping("/{recruit-post-id}/expired")
     public ResponseEntity expiredRecruitPost (@PathVariable("recruit-post-id")long id){
+        log.info("RecruitPost : {}",id);
         service.expiredRecruitPost(id);
+        log.info("Expired RecruitPost :{}",id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
